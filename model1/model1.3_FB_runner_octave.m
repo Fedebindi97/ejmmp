469667e158e492b6c2eec6bde656b1343b175b18
fedebindi <82228286+Fedebindi97@users.noreply.github.com>
2024-10-09 08:19:15 +0200

    Integrating Octave and Python code
    Restructuring code so as to allow for Octave (demand) - Python (supply) integration. Now, python code is in a .py file, and there is a new notebook allowing for the concurrent running of octave and python scripts.

--- model1/model1.3_FB_runner_octave.m
+++ model1/model1.3_FB_runner_octave.m
@@ -1,5 +1,8 @@
+% Clear redis database
+system('/opt/homebrew/bin/redis-cli flushdb');
+
 % Define the number of firms
-number_of_firms = 10000; % You can modify this value to change the number of firms; eventually, this will need to reflect the Firm.count parameter in model1.yaml
+number_of_firms = 10000; % You can modify this value to change the number of firms; eventually, this will need to reflect the 'Firm.count' parameter in model1.yaml
 
 for i = 1:264 % 'howManyCycles' parameter in model1.yaml
     % Wait until Python has sent data
@@ -10,6 +13,12 @@ for i = 1:264 % 'howManyCycles' parameter in model1.yaml
         endif
         pause(1); % Wait before checking again
     end
+
+     % Process data
+    printf("Octave received: %s\n", strtrim(data)); 
+    
+    % Clear the python_to_octave key to avoid stale reads
+    system('/opt/homebrew/bin/redis-cli del python_to_octave');
 
     % Generate an array of number_of_firms random floats between 0 and 1
     rand_floats = rand(1, number_of_firms);
@@ -30,10 +39,10 @@ for i = 1:264 % 'howManyCycles' parameter in model1.yaml
     index_start = strfind(data, "Start");
     if !isempty(index) || !isempty(index_start)
         % Prepare the result to send both arrays as a single string
-        printf('Octave computing...')
+        printf(sprintf('Octave computing step %s...', num2str(i)));
         pause(2); % Simulate processing time - demand code will go here
-        result = sprintf('Step: [%s], Floats: [%s], Integers: [%s]', num2str(i), float_str, int_str);
-        printf('Octave sending arrays of number_of_firms floats and number_of_firms integers\n');
+        result = sprintf('Step: %s, Floats: [%s], Integers: [%s]', num2str(i), float_str, int_str);
+        printf(sprintf('Octave sending step %s\n', num2str(i)));
         redis_command = ['echo "', result, '" | /opt/homebrew/bin/redis-cli -x set octave_to_python'];
         system(redis_command);
     end
